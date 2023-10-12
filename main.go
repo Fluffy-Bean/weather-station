372d384b4ce585c9c8322eca1b61072ef71c4e70
Fluffy-Bean <michal-gdula@protonmail.com>
2023-10-12 21:11:20 +0100

    Add devices table

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 var Database *sql.DB
@@ -20,6 +21,11 @@ type WeatherResponse struct {
 	Temperature float64
 	Humidity    float64
 	Pressure    float64
+}
+type DeviceResponse struct {
+	Id    int
+	Name  string
+	Model string
 }
 type ErrorResponse struct {
 	Message string
@@ -35,6 +41,7 @@ func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", handleRoot)
 	mux.HandleFunc("/upload", handleUpload)
+	mux.HandleFunc("/devices", handleDevices)
 
 	log.Fatal(http.ListenAndServe("localhost:8080", mux))
 }
@@ -53,6 +60,7 @@ func CreateTables() {
 		CREATE TABLE IF NOT EXISTS devices (
 			id INTEGER PRIMARY KEY,
 			name TEXT,
+			model TEXT,
 			created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 		)
 	`)
@@ -65,6 +73,7 @@ func CreateTables() {
 func handleRoot(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("Request for /")
 	writer.Header().Set("Content-Type", "application/json")
+	writer.Header().Set("Access-Control-Allow-Origin", "*")
 
 	statement, _ := Database.Prepare("SELECT id, temperature, humidity, pressure FROM weather ORDER BY created_at DESC")
 	rows, _ := statement.Query()
@@ -82,12 +91,15 @@ func handleRoot(writer http.ResponseWriter, request *http.Request) {
 		responseData = append(responseData, WeatherResponse{id, temperature, humidity, pressure})
 	}
 
+	time.Sleep(3 * time.Second)
+
 	json.NewEncoder(writer).Encode(Response{responseData})
 }
 
 func handleUpload(writer http.ResponseWriter, request *http.Request) {
 	fmt.Println("Request for /upload")
 	writer.Header().Set("Content-Type", "application/json")
+	writer.Header().Set("Access-Control-Allow-Origin", "*")
 
 	temperature, errTemp := strconv.ParseFloat(request.URL.Query().Get("temperature"), 64)
 	humidity, errHumid := strconv.ParseFloat(request.URL.Query().Get("humidity"), 64)
@@ -108,4 +120,29 @@ func handleUpload(writer http.ResponseWriter, request *http.Request) {
 	}
 
 	json.NewEncoder(writer).Encode(Response{WeatherResponse{0, temperature, humidity, pressure}})
+}
+
+func handleDevices(writer http.ResponseWriter, request *http.Request) {
+	fmt.Println("Request for /devices")
+	writer.Header().Set("Content-Type", "application/json")
+	writer.Header().Set("Access-Control-Allow-Origin", "*")
+
+	statement, _ := Database.Prepare("SELECT id, name, model FROM devices")
+	rows, _ := statement.Query()
+
+	var (
+		responseData []DeviceResponse
+		id           int
+		name         string
+		model        string
+	)
+
+	for rows.Next() {
+		rows.Scan(&id, &name, &model)
+		responseData = append(responseData, DeviceResponse{id, name, model})
+	}
+
+	time.Sleep(3 * time.Second)
+
+	json.NewEncoder(writer).Encode(Response{responseData})
 }
