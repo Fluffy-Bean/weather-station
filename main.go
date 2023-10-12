0f3597aa9d1ae6f333d53a6e1f183375bf96a126
Fluffy-Bean <michal-gdula@protonmail.com>
2023-10-12 19:47:29 +0100

    Respond with Json data

--- main.go
+++ main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/mattn/go-sqlite3"
-	"io"
+	"log"
 	"net/http"
 	"strconv"
-	"strings"
 )
 
 var Database *sql.DB
+
+type Response struct {
+	Data interface{}
+}
+type WeatherResponse struct {
+	Id          int
+	Temperature float64
+	Humidity    float64
+	Pressure    float64
+}
+type ErrorResponse struct {
+	Message string
+	Code    int
+}
 
 func main() {
 	database, _ := sql.Open("sqlite3", "./weather.db")
@@ -22,67 +36,76 @@ func main() {
 	mux.HandleFunc("/", handleRoot)
 	mux.HandleFunc("/upload", handleUpload)
 
-	http.ListenAndServe("localhost:8080", mux)
+	log.Fatal(http.ListenAndServe("localhost:8080", mux))
 }
 
 func CreateTables() {
-	query := "CREATE TABLE IF NOT EXISTS weather (id INTEGER PRIMARY KEY, temperature REAL, humidity REAL, pressure REAL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP)"
-	statement, _ := Database.Prepare(query)
-	statement.Exec()
+	weatherTable, _ := Database.Prepare(`
+		CREATE TABLE IF NOT EXISTS weather (
+			id INTEGER PRIMARY KEY,
+			temperature REAL,
+			humidity REAL,
+			pressure REAL,
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+		)
+	`)
+	devicesTable, _ := Database.Prepare(`
+		CREATE TABLE IF NOT EXISTS devices (
+			id INTEGER PRIMARY KEY,
+			name TEXT,
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+		)
+	`)
+
+	fmt.Println("Creating tables...")
+	weatherTable.Exec()
+	devicesTable.Exec()
 }
 
 func handleRoot(writer http.ResponseWriter, request *http.Request) {
-	query := "SELECT id, temperature, humidity, pressure FROM weather ORDER BY created_at DESC"
-	statement, _ := Database.Prepare(query)
+	fmt.Println("Request for /")
+	writer.Header().Set("Content-Type", "application/json")
+
+	statement, _ := Database.Prepare("SELECT id, temperature, humidity, pressure FROM weather ORDER BY created_at DESC")
 	rows, _ := statement.Query()
 
 	var (
-		id          int
-		temperature float64
-		humidity    float64
-		pressure    float64
-		data        []string
+		responseData []WeatherResponse
+		id           int
+		temperature  float64
+		humidity     float64
+		pressure     float64
 	)
 
 	for rows.Next() {
-		err := rows.Scan(&id, &temperature, &humidity, &pressure)
-		if err != nil {
-			continue
-		}
-		data = append(data, fmt.Sprintf("<tr><td>%v</td><td>%f</td><td>%f</td><td>%f</td></tr>", id, temperature, humidity, pressure))
+		rows.Scan(&id, &temperature, &humidity, &pressure)
+		responseData = append(responseData, WeatherResponse{id, temperature, humidity, pressure})
 	}
 
-	html := fmt.Sprintf("<h1>Weather</h1><table><tr><th>ID</th><th>Temperature</th><th>Humidity</th><th>Pressure</th></tr>%s</table>", strings.Join(data, ""))
-
-	io.WriteString(writer, html)
+	json.NewEncoder(writer).Encode(Response{responseData})
 }
 
 func handleUpload(writer http.ResponseWriter, request *http.Request) {
-	temperature, err := strconv.ParseFloat(request.URL.Query().Get("temperature"), 64)
-	if err != nil {
-		io.WriteString(writer, "<p>Invalid temperature</p>")
-		return
-	}
-	humidity, err := strconv.ParseFloat(request.URL.Query().Get("humidity"), 64)
-	if err != nil {
-		io.WriteString(writer, "<p>Invalid humidity</p>")
-		return
-	}
-	pressure, err := strconv.ParseFloat(request.URL.Query().Get("pressure"), 64)
-	if err != nil {
-		io.WriteString(writer, "<p>Invalid pressure</p>")
+	fmt.Println("Request for /upload")
+	writer.Header().Set("Content-Type", "application/json")
+
+	temperature, errTemp := strconv.ParseFloat(request.URL.Query().Get("temperature"), 64)
+	humidity, errHumid := strconv.ParseFloat(request.URL.Query().Get("humidity"), 64)
+	pressure, errPress := strconv.ParseFloat(request.URL.Query().Get("pressure"), 64)
+
+	if errTemp != nil || errHumid != nil || errPress != nil {
+		writer.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(writer).Encode(Response{ErrorResponse{"Invalid data", 400}})
 		return
 	}
 
-	query := "INSERT INTO weather (temperature, humidity, pressure) VALUES (?, ?, ?)"
-	statement, _ := Database.Prepare(query)
-	_, err = statement.Exec(temperature, humidity, pressure)
+	statement, _ := Database.Prepare("INSERT INTO weather (temperature, humidity, pressure) VALUES (?, ?, ?)")
+	_, err := statement.Exec(temperature, humidity, pressure)
 	if err != nil {
-		io.WriteString(writer, "<p>Failed to insert data</p>")
+		writer.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(writer).Encode(Response{ErrorResponse{"Internal server error", 500}})
 		return
 	}
 
-	html := fmt.Sprintf("<h1>Weather</h1><p>Temperature: %f</p><p>Humidity: %f</p><p>Pressure: %f</p>", temperature, humidity, pressure)
-
-	io.WriteString(writer, html)
+	json.NewEncoder(writer).Encode(Response{WeatherResponse{0, temperature, humidity, pressure}})
 }
