7d688fa557425403ee87e11bf5ba1a09ff4b0674
Fluffy-Bean <michal-gdula@protonmail.com>
2023-10-14 13:21:40 +0100

    Bind form data to struct

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	uuid4 "github.com/google/uuid" // Stupid way to avoid conflict
 	_ "github.com/mattn/go-sqlite3"
 	"log"
-	"strconv"
 )
 
 var database *sql.DB
@@ -18,12 +17,23 @@ type WeatherResponse struct {
 	Humidity    float64
 	Pressure    float64
 }
+type WeatherForm struct {
+	Uuid        string  `form:"uuid" json:"uuid" binding:"required"`
+	Temperature float64 `form:"temperature" json:"temperature" binding:"required"`
+	Humidity    float64 `form:"humidity" json:"humidity" binding:"required"`
+	Pressure    float64 `form:"pressure" json:"pressure" binding:"required"`
+}
 type DeviceResponse struct {
 	Id       int
 	Name     string
 	Address  string
 	Version  string
 	Location string
+}
+type DeviceForm struct {
+	Name    string `form:"name" json:"name" binding:"required"`
+	Version string `form:"version" json:"version" binding:"required"`
+	Address string `form:"address" json:"address" binding:"required"`
 }
 
 func main() {
@@ -32,27 +42,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	CreateTables()
 
-	r := gin.Default()
-	r.GET("/", indexGet)
-	r.POST("/", indexPost)
-	r.GET("/devices", devicesGet)
-	r.POST("/devices", devicesPost)
-
-	log.Fatal(r.Run())
-}
-
-func CreateTables() {
-	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS weather (id INTEGER PRIMARY KEY AUTOINCREMENT, temperature REAL, humidity REAL, pressure REAL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP);")
+	// Make Weather table
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS weather (id INTEGER PRIMARY KEY AUTOINCREMENT, temperature REAL, humidity REAL, pressure REAL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP);")
 	if err != nil {
 		log.Fatal(err)
 	}
-	_, err = statement.Exec()
+	_, _ = statement.Exec()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Make Devices table
 	statement, err = database.Prepare("CREATE TABLE IF NOT EXISTS devices (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, address TEXT, version TEXT, uuid TEXT, created_at DATETIME DEFAULT CURRENT_TIMESTAMP);")
 	if err != nil {
 		log.Fatal(err)
@@ -61,6 +62,15 @@ func CreateTables() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Run HTTP server
+	r := gin.Default()
+	r.GET("/", indexGet)
+	r.POST("/", indexPost)
+	r.GET("/devices", devicesGet)
+	r.POST("/devices", devicesPost)
+
+	log.Fatal(r.Run(":8080"))
 }
 
 func indexGet(c *gin.Context) {
@@ -69,10 +79,12 @@ func indexGet(c *gin.Context) {
 	statement, err := database.Prepare("SELECT id, temperature, humidity, pressure FROM weather ORDER BY created_at DESC;")
 	if err != nil {
 		c.JSON(500, gin.H{"error": "Internal server error"})
+		return
 	}
 	row, err := statement.Query()
 	if err != nil {
 		c.JSON(500, gin.H{"error": "Internal server error"})
+		return
 	}
 
 	var (
@@ -99,50 +111,40 @@ func indexGet(c *gin.Context) {
 func indexPost(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 
-	uuid := c.PostForm("uuid")
+	var form WeatherForm
+	if err := c.ShouldBind(&form); err != nil {
+		fmt.Println("Error binding form")
+		c.JSON(400, gin.H{"error": "Bad request"})
+		return
+	}
 
 	statement, err := database.Prepare("SELECT id FROM devices WHERE uuid = ? LIMIT 1;")
-	row, err := statement.Query(uuid)
+	row, err := statement.Query(form.Uuid)
 	if err != nil {
 		fmt.Println("Error checking if device exists")
 		c.JSON(500, gin.H{"error": "Internal server error"})
+		return
 	}
 	if !row.Next() {
-		fmt.Println("Device does not exist")
+		fmt.Println("Device does not exist" + form.Uuid)
 		c.JSON(403, gin.H{"error": "Device does not exist"})
+		return
 	}
 
 	_ = statement.Close()
 	_ = row.Close()
-
-	temperature, err := strconv.ParseFloat(c.PostForm("temperature"), 64)
-	if err != nil {
-		fmt.Printf("Error parsing temperature %v\n", temperature)
-		c.JSON(400, gin.H{"error": "Bad Request"})
-	}
-
-	humidity, err := strconv.ParseFloat(c.PostForm("humidity"), 64)
-	if err != nil {
-		fmt.Println("Error parsing humidity " + c.PostForm("humidity"))
-		c.JSON(400, gin.H{"error": "Bad Request"})
-	}
-
-	pressure, err := strconv.ParseFloat(c.PostForm("pressure"), 64)
-	if err != nil {
-		fmt.Println("Error parsing pressure " + c.PostForm("pressure"))
-		c.JSON(400, gin.H{"error": "Bad Request"})
-	}
 
 	statement, _ = database.Prepare("INSERT INTO weather (temperature, humidity, pressure) VALUES (?, ?, ?);")
-	_, err = statement.Exec(temperature, humidity, pressure)
+	_, err = statement.Exec(form.Temperature, form.Humidity, form.Pressure)
 	if err != nil {
 		c.JSON(500, gin.H{"error": "Internal server error"})
+		return
 	}
 
 	_ = statement.Close()
 	_ = row.Close()
 
-	c.JSON(200, WeatherResponse{0, temperature, humidity, pressure})
+	c.JSON(200, WeatherResponse{0, form.Temperature, form.Humidity, form.Pressure})
 }
 
 func devicesGet(c *gin.Context) {
@@ -151,10 +153,12 @@ func devicesGet(c *gin.Context) {
 	statement, err := database.Prepare("SELECT id, name, address, version FROM devices;")
 	if err != nil {
 		c.JSON(500, gin.H{"error": "Internal server error"})
+		return
 	}
 	row, err := statement.Query()
 	if err != nil {
 		c.JSON(500, gin.H{"error": "Internal server error"})
+		return
 	}
 
 	var (
@@ -181,31 +185,35 @@ func devicesGet(c *gin.Context) {
 func devicesPost(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 
-	name := c.PostForm("name")
-	version := c.PostForm("version")
-	address := c.PostForm("address")
+	var form DeviceForm
+	if err := c.ShouldBind(&form); err != nil {
+		c.JSON(400, gin.H{"error": "Bad request"})
+		return
+	}
 
 	statement, err := database.Prepare("SELECT uuid FROM devices WHERE address = ? LIMIT 1;")
-	row, err := statement.Query(address)
+	row, err := statement.Query(form.Address)
 	if err != nil {
 		fmt.Println("Error checking if device exists")
 		c.JSON(500, gin.H{"error": "Internal server error"})
+		return
 	}
 
 	var uuid string
-
 	if row.Next() {
 		err = row.Scan(&uuid)
 		if err != nil {
 			fmt.Println("Error scanning row")
 			c.JSON(500, gin.H{"error": "Internal server error"})
+			return
 		}
 	} else {
 		uuid = uuid4.NewString()
 		statement, _ = database.Prepare("INSERT INTO devices (name, version, address, uuid) VALUES (?, ?, ?, ?);")
-		_, err = statement.Exec(name, version, address, uuid)
+		_, err = statement.Exec(form.Name, form.Version, form.Address, uuid)
 		if err != nil {
 			c.JSON(500, gin.H{"error": "Internal server error"})
+			return
 		}
 	}
 
